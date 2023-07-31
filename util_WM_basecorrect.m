95ca5acb7ee46497a3a2b39a9ca1afdaae0d811d
sjeung <seinjeung@gmail.com>
2023-07-31 17:13:41 +0200

    Remove baseline outlier segments

--- util_WM_basecorrect.m
+++ util_WM_basecorrect.m
@@ -4,11 +4,19 @@ function util_WM_basecorrect(ERSPdata, ERSPbase, Pi, condText)
 WM_config;
 
 % issue warning if freq points differ too much
-timeDiff        = abs(ERSPbase.freq - ERSPdata.freq); 
-assert(~any(timeDiff > 0.3));                                               % difference frequencies should be under 0.3 Hz
+timeDiff        = abs(ERSPbase.freq - ERSPdata.freq);
+if any(timeDiff > 0.3)
+    error('baseline and data frequency points differ too much');            % difference frequencies should be under 0.3 Hz
+end
 
-% average baseilne data over trials and samples 
-baseMean    = mean(mean(ERSPbase.powspctrm, 4, 'omitnan'),1,'omitnan');
+% remove outliers from baseline trials 
+baseTrials  = mean(ERSPbase.powspctrm, [2,3,4], 'omitnan'); 
+[~,outInds] = rmoutliers(baseTrials);                                       % extract outlier indices
+ 
+% average baseline data over trials and samples 
+powerTrials                = mean(ERSPbase.powspctrm, 4, 'omitnan');        % trials X channels X freqs
+powerTrials(outInds,:,:)   = [];                                            % take out outlier trials
+baseMean    = mean(powerTrials,1,'omitnan');                                % average over trials
 baseMat     = repmat(baseMean, size(ERSPdata.powspctrm,1),1,1,size(ERSPdata.powspctrm,4));
 
 ERSPcorr                        = ERSPdata;                                 % copy data structure of the input                              
