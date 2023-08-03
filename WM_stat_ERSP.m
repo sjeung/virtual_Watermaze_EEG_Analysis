b6dc741270e819eb2480d9ff95d3118c1799efd3
sjeung <seinjeung@gmail.com>
2023-08-03 13:36:03 +0200

    Add band power stats

--- WM_stat_ERSP.m
+++ WM_stat_ERSP.m
@@ -8,17 +8,20 @@ function [p_values] = WM_stat_ERSP(trialType, trialSection, channelGroup)
 
 % Parameters
 %--------------------------------------------------------------------------
+WM_config; 
 pThreshold      = 0.05; 
 nPermutations   = 2048; 
-frequencyBands  = [];
 
-
+% Load data
+%--------------------------------------------------------------------------
 ERSPvar = load(['P:\Sein_Jeung\Project_Watermaze\WM_EEG_Results\ERSP\sub-81001\sub-81001_' trialType '_' channelGroup.key '_' trialSection '_ERSP.mat'], ['ERSP' trialSection]);
 fn = fieldnames(ERSPvar); vn = fn{1}; ERSP = ERSPvar.(vn); % highly annoying to work with this variable name - update later
 
 timePoints = ERSP.time; 
 freqPoints = ERSP.freq;
 
+% Main loop 
+%--------------------------------------------------------------------------
 % participants to include or exclude
 patientIDs      = 81001:81011; 
 controlIDs      = [82001:82011, 83001:83011, 84009];
@@ -31,16 +34,36 @@ missedPatients  = [];
 missedControls  = []; 
 ERSPp           = {};
 ERSPc           = {};
- 
+bandpowersp     = [];   % matrix, number of patients X number of frequency bands
+bandpowersc     = [];
+    
 for Pi = patientIDs
     try
-        ERSPvar         = load(['P:\Sein_Jeung\Project_Watermaze\WM_EEG_Results\ERSP_pruned\sub-' num2str(Pi) '\sub-' num2str(Pi) '_' trialType '_' channelGroup.key '_' trialSection '_ERSP_pruned.mat']);
-       
+        ERSPvar         = load(['P:\Sein_Jeung\Project_Watermaze\WM_EEG_Results\ERSP_pruned\sub-' num2str(Pi) '\sub-' num2str(Pi) '_' trialType '_' channelGroup.key '_' trialSection '_ERSP_pruned.mat']);    
         fn              = fieldnames(ERSPvar);
         vn              = fn{1};
         ERSP            = ERSPvar.(vn);
         ERSPp{end+1}    = squeeze(mean(ERSP.powspctrm,[1,2],'omitnan'));
         
+        % compute band power
+        bandPowers      = []; % participant specific, trial-by-trial powers  
+        meanBandPowers  = []; % trials are averaged
+        
+        for Bi = 1:numel(config_param.band_names) 
+
+            % find indices of closed point to upper and lower bounds
+            [~,lowInd]                  = min(abs(freqPoints - config_param.band_bounds(Bi,1)));
+            [~,upInd]                   = min(abs(freqPoints - config_param.band_bounds(Bi,2)));
+            bandPowers(:,end+1)         = mean(ERSP.powspctrm(:,:,lowInd:upInd,:),[2,3,4]);
+            meanBandPowers(Bi)          = squeeze(mean(ERSP.powspctrm(:,:,lowInd:upInd,:),[1,2,3,4])); 
+            
+        end
+        
+        bandpowersp(end+1,:)       = meanBandPowers;
+        
+        % save bandpowers to be put against performance measure 
+        save(fullpath(),'bandpowers')
+
     catch
         missedPatients(end+1) = Pi;
     end
@@ -55,11 +78,29 @@ for Pi = controlIDs
         ERSP            = ERSPvar.(vn);
         ERSPc{end+1}    = squeeze(mean(ERSP.powspctrm,[1,2], 'omitnan'));
         
+        % compute band power
+        bandPowers      = []; % participant specific, trial-by-trial powers
+        meanBandPowers  = []; % trials are averaged
+        
+        for Bi = 1:numel(config_param.band_names)
+            
+            % find indices of closed point to upper and lower bounds
+            [~,lowInd]                  = min(abs(freqPoints - config_param.band_bounds(Bi,1)));
+            [~,upInd]                   = min(abs(freqPoints - config_param.band_bounds(Bi,2)));
+            bandPowers(:,end+1)         = mean(ERSP.powspctrm(:,:,lowInd:upInd,:),[2,3,4]);
+            meanBandPowers(Bi)          = squeeze(mean(ERSP.powspctrm(:,:,lowInd:upInd,:),[1,2,3,4]));
+            
+        end
+        
+        bandpowersc(end+1,:)            = meanBandPowers;
+        
+        [bandFileName, bandFileDir]     = assemble_file(config_folder.results_folder, config_folder.band_powers_folder, ['_' trialType '_' trialSection '_' channelGroup.key '_' config_folder.bandPowerFileName], Pi);
+        save(fullfile(bandFileDir, bandFileName), 'bandpowers')
+        
     catch
        missedControls(end+1) = Pi; 
    end
 end
-
 
 pMat    = cat(3,ERSPp{:});
 cMat    = cat(3,ERSPc{1:numel(ERSPc)});
@@ -67,39 +108,54 @@ cMat    = cat(3,ERSPc{1:numel(ERSPc)});
 
 % Participants versus controls contrast
 %--------------------------------------------------------------------------
-
 [clusters, p_values, t_sums, permutation_distribution] = permutest(pMat,cMat, false, pThreshold, nPermutations, true);
 
 util_WM_plot_ERSP(ERSPp, timePoints, freqPoints, ['ERSP_MTL_' channelGroup.key], ['P:\Sein_Jeung\Project_Watermaze\WM_EEG_Results\ERSP\aggregated_ERSP_mtl_' trialType '_' channelGroup.key '_' trialSection '.png'], [])
 util_WM_plot_ERSP(ERSPc, timePoints, freqPoints, ['ERSP_CTRL_' channelGroup.key], ['P:\Sein_Jeung\Project_Watermaze\WM_EEG_Results\ERSP\aggregated_ERSP_ctrl_' trialType '_' channelGroup.key '_' trialSection '.png'], [])
 
 
-% Baseline versus navigation spectra 
+% Participant versus control spectra 
 %--------------------------------------------------------------------------
-
 figure; 
-plot()
-pMat
-
-
+this = mean(pMat, [2,3]); this = squeeze(this);
+plot(this, 'b', 'LineWidth', 2);
+hold on; 
+that = mean(cMat, [2,3]); that = squeeze(that);
+plot(that, 'k', 'LineWidth', 2)
+legend('mtl', 'ctrl')
+xticklabelsCell = arrayfun(@num2str, round(freqPoints), 'UniformOutput', false);
+xticklabels(xticklabelsCell);
+xlabel('frequencies in Hz')
+ylabel('trial power / baseline')
+title(['Spectra ' trialType ', ' channelGroup.key ', ' trialSection], 'Interpreter', 'none')
 
 % Band power 
 %--------------------------------------------------------------------------
+figure; 
+dataBoxPlot         = nan(3,2,30);
+dataBoxPlot(:,1,1:size(bandpowersp,1))  = bandpowersp';
+dataBoxPlot(:,2,1:size(bandpowersc,1))  = bandpowersc';
+boxplot2(dataBoxPlot); 
 
+h =  findobj(gca,'Tag','Box');
 
+for j=1:length(h)
 
+    if mod(j,2) == 1
+        color = 'b';
+    else
+        color = [255, 204, 51]/(256);
+    end
+    
+    patch(get(h(j),'XData'),get(h(j),'YData'),color,'FaceAlpha',.3, 'EdgeColor','w');
+    
+end
 
-% Regresssion 
-%--------------------------------------------------------------------------
-
-
-
-% Save and summarize results 
-%--------------------------------------------------------------------------
-
-
-
-
+for Bi = 1:numel(config_param.band_names)
+    [~, pval] = ttest2(bandpowersp(:,Bi), bandpowersc(:,Bi)); 
+    disp([config_param.band_names{Bi} ', ' trialType ', ' trialSection ', ' channelGroup.key ' band powers'])
+    disp(['P = ' num2str(pval)])
+end
 
 % Attempted use of fieldtrip built-in statistics - not working due to
 % imbalanced design 
